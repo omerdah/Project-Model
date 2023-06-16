5d5ec6189bcbc671bd2c703bb8a4f13dd9b70be2
omerdah <88436135+omerdah@users.noreply.github.com>
2023-06-16 12:16:30 +0300

    Update RF_regressor_model.py

--- RF_regressor_model.py
+++ RF_regressor_model.py
@@ -58,7 +58,9 @@ def main():
         if feature_name in input_names:
             valid_values = list(df_mappings[feature_name].dropna().unique())
             if feature_name == 'מועד זריעה':
-                input_value = st.date_input(feature_name)
+                min_date = datetime.datetime.now().date()
+                max_date = pd.to_datetime('2030-12-31')
+                input_value = st.date_input(feature_name, min_value=min_date, max_value=max_date)
             elif isinstance(valid_values, list):
                 input_value = st.selectbox(feature_name, valid_values)
             else:
